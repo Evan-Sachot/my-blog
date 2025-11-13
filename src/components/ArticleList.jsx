653a493b4f0ad871bb9a5566bf01531723515ee5
evan-sachot <evan.sachot@laplateforme.io>
2025-11-13 16:11:09 +0100

    ok

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,19 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import ArticleThumbnail from "./ArticleThumbnail";
 import "./ArticleList.css";
 
 function ArticleList({ articles }) {
+     const [searchTerm,setSearchTerm] =useState("")
+
+     const [fetchedArticles, setFetchedArticles]= useState(articles)
+     
+    useEffect(( )=>{
+        const url = "http://localhost:3001/articles"+ searchTerm
+    
+
+        fetch(url)
+        .then((response)=>{
+            if(!response.ok){
+                throw new Error ('echec de connexion')
+            }return response.json()
+        })
+       
+      .then((data)=>{
+        setFetchedArticles(data);
+      
+      })
+    },[searchTerm])
+   
+    const filtered = fetchedArticles.filter(article =>
+    article.title.toLowerCase().includes(searchTerm.toLowerCase())|| article.content.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+);
+
+
   return (
     <section className="article-list">
-      <h2>Articles récents</h2>
+      <h2>Truc que j'aime bien</h2>
+      <input type="text" className="filter" value={searchTerm} onChange={e => setSearchTerm(e.target.value)}
+      placeholder="Rechercher un articles" />
       <div className="articles-container">
-        {articles.map(article => (
+        {filtered.map(article => (
           <ArticleThumbnail
             key={article.id}
-            title={article.title}
-            summary={article.summary}
+            title={article ? article.title : "chargemnet..."}
+            content={article ? article.content : "chargement..."}
             link={article.link}
-            image={article.image}
+            image={article ?  article.image : "chargement..."}
           />
         ))}
       </div>
--- src/components/ArticleThumbnail.jsx
+++ src/components/ArticleThumbnail.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import "./ArticleThumbnail.css";
+import { useState } from "react";
 
-function ArticleThumbnail({ title, summary, link, image }) {
+function ArticleThumbnail({ title, content, link, image }) {
+    const [liked,setliked] =useState(false);
+    const togglelike = () =>{
+        setliked(!liked)
+    }
   return (
+
     <div className="article-thumbnail">
+        
       {image && <img src={image} alt={title} className="thumbnail-image" />}
       <h3>{title}</h3>
-      <p>{summary}</p>
+      
+      <p>{content}</p>
       <a href={link} className="read-more">Lire la suite</a>
+      <button
+      className={`like-button ${liked ? "liked" : ""}`}
+      onClick={togglelike}>
+        {liked?"❤️":"♡"}
+        </button>
     </div>
   );
 }
